9fba7539f32091d56519d6a830bf97da854bd2c5
breakpoint-nedimalicusic <156081023+breakpoint-nedimalicusic@users.noreply.github.com>
2025-03-18 14:35:21 +0100

    Update PlatformDeviceIdPlugin.kt

--- platform_device_id/android/src/main/kotlin/com/di1shuai/platform_device_id/PlatformDeviceIdPlugin.kt
+++ platform_device_id/android/src/main/kotlin/com/di1shuai/platform_device_id/PlatformDeviceIdPlugin.kt
@@ -22,23 +22,6 @@ public class PlatformDeviceIdPlugin: FlutterPlugin, MethodCallHandler {
     channel.setMethodCallHandler(this);
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "platform_device_id")
-      channel.setMethodCallHandler(PlatformDeviceIdPlugin())
-    }
-  }
-
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
     if (call.method == "getPlatformVersion") {
       result.success("Android ${android.os.Build.VERSION.RELEASE}")
